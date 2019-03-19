51242aa8543dc55a6d38ca17af06c17a60d1ee44
Carlos Ferreira <conta@especializati.com.br>
2019-03-19 16:47:40 -0300

    Middleware APIs

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,14 +3,17 @@
 Route::post('login', 'Api\Auth\LoginController@login');
 Route::post('register', 'Api\Auth\LoginController@register');
 
-Route::apiResource('/users', 'Api\UserController');
-Route::apiResource('/profiles', 'Api\ProfileController');
-Route::apiResource('/permissions', 'Api\PermissionController');
 
-Route::get('/user/{id}/profiles', 'Api\ProfileUserController@userProfiles')->name('user.profiles');
-Route::get('/user/{id}/profiles-not-linked', 'Api\ProfileUserController@profilesNotLinkedUser')->name('user.profiles.not.linked');
-Route::post('/user/{id}/profiles', 'Api\ProfileUserController@userAddProfiles')->name('user.add.profiles');
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::apiResource('/users', 'Api\UserController');
+    Route::apiResource('/profiles', 'Api\ProfileController');
+    Route::apiResource('/permissions', 'Api\PermissionController');
 
-Route::get('/profile/{id}/permissions', 'Api\PermissionProfileController@profilePermissions')->name('profile.permissions');
-Route::get('/profile/{id}/permissions-not-linked', 'Api\PermissionProfileController@permissionsNotLinkedProfile')->name('profile.permissions.not.linked');
-Route::post('/profile/{id}/permissions', 'Api\PermissionProfileController@profileAddPermissions')->name('profile.add.permissions');
+    Route::get('/user/{id}/profiles', 'Api\ProfileUserController@userProfiles')->name('user.profiles');
+    Route::get('/user/{id}/profiles-not-linked', 'Api\ProfileUserController@profilesNotLinkedUser')->name('user.profiles.not.linked');
+    Route::post('/user/{id}/profiles', 'Api\ProfileUserController@userAddProfiles')->name('user.add.profiles');
+
+    Route::get('/profile/{id}/permissions', 'Api\PermissionProfileController@profilePermissions')->name('profile.permissions');
+    Route::get('/profile/{id}/permissions-not-linked', 'Api\PermissionProfileController@permissionsNotLinkedProfile')->name('profile.permissions.not.linked');
+    Route::post('/profile/{id}/permissions', 'Api\PermissionProfileController@profileAddPermissions')->name('profile.add.permissions');
+});
