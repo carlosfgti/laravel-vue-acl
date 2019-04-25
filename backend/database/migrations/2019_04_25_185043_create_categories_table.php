144c3719ef2d6186198471b44572302cb08afa25
Carlos Ferreira <conta@especializati.com.br>
2019-04-25 15:54:40 -0300

    Structure of table and fillable

--- backend/database/migrations/2019_04_25_185043_create_categories_table.php
+++ backend/database/migrations/2019_04_25_185043_create_categories_table.php
@@ -15,6 +15,8 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('title')->unique();
+            $table->string('slug')->unique();
             $table->timestamps();
         });
     }
