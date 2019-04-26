51366008fdaa195c8f96be423062160dac34c746
Carlos Ferreira <conta@especializati.com.br>
2019-04-26 15:19:02 -0300

    Api Categories

---
+++ backend/app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Requests\StoreUpdateProfileRequest;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Http\Resources\CategoryResource;
+
+class CategoryController extends Controller
+{
+    private $repository;
+
+    public function __construct(Category $category)
+    {
+        $this->repository = $category;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $categories = $this->repository->paginate();
+
+        return CategoryResource::collection($categories);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreUpdateProfileRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $category = $this->repository->create($request->all());
+
+        return (new CategoryResource($category))
+                ->response()
+                ->setStatusCode(201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $category = $this->repository->findOrFail($id);
+
+        return new CategoryResource($category);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreUpdateProfileRequest  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $category = $this->repository->findOrFail($id);
+
+        $category->update($request->all());
+
+        return new CategoryResource($category);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $this->repository->findOrFail($id)->delete();
+
+        return response()->json(null, 204);
+    }
+}
