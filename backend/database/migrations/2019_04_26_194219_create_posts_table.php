ec883519c3ba141d55e3a3572f0f72cdbb6db566
Carlos Ferreira <conta@especializati.com.br>
2019-04-26 16:46:25 -0300

    Post: Model and migration file

---
+++ backend/database/migrations/2019_04_26_194219_create_posts_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('category_id');
+            $table->string('title')->unique();
+            $table->string('slug')->unique();
+            $table->string('body');
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                    ->references('id')
+                    ->on('users');
+            $table->foreign('category_id')
+                    ->references('id')
+                    ->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
