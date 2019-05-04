678250380467e17c097f71c82b8d6798f01c246e
Carlos Ferreira <conta@especializati.com.br>
2019-05-04 16:05:47 -0300

    Vuex User

--- front-vue/src/main.js
+++ front-vue/src/main.js
@@ -3,11 +3,13 @@ import App from './App.vue'
 import router from './routers'
 import './registerServiceWorker'
 import ArgonDashboard from './plugins/argon-dashboard'
+import store from './store'
 
 Vue.config.productionTip = false
 
 Vue.use(ArgonDashboard)
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
