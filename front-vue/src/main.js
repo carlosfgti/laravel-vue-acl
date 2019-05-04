4807d6a5bf97e074115d58b5356df2c466529c80
Carlos Ferreira <conta@especializati.com.br>
2019-05-04 15:29:20 -0300

    Organized Routes

--- front-vue/src/main.js
+++ front-vue/src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './routers'
 import './registerServiceWorker'
 import ArgonDashboard from './plugins/argon-dashboard'
 
---
+++ front-vue/src/routers/index.js
@@ -0,0 +1,40 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import DashboardLayout from '@/layout/DashboardLayout'
+import AuthLayout from '@/layout/AuthLayout'
+Vue.use(Router)
+
+export default new Router({
+  linkExactActiveClass: 'active',
+  routes: [
+    {
+      path: '/',
+      redirect: 'dashboard',
+      component: DashboardLayout,
+      children: [
+        {
+          path: '/dashboard',
+          name: 'dashboard',
+          component: () => import('@/views/Dashboard.vue')
+        },
+      ]
+    },
+    {
+      path: '/',
+      redirect: 'login',
+      component: AuthLayout,
+      children: [
+        {
+          path: '/login',
+          name: 'login',
+          component: () => import('@/views/Login.vue')
+        },
+        {
+          path: '/register',
+          name: 'register',
+          component: () => import('@/views/Register.vue')
+        }
+      ]
+    }
+  ]
+})
